eda0c54db3ada17e1a9dc602ef89dd70cd4f2b11
Mininator <164217898+Mininator@users.noreply.github.com>
2026-03-27 19:28:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById("stars");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let stars = [];
+for(let i=0;i<200;i++){
+  stars.push({
+    x:Math.random()*canvas.width,
+    y:Math.random()*canvas.height,
+    r:Math.random()*2,
+    speed:Math.random()*0.5
+  });
+}
+
+function draw(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.fillStyle="white";
+
+  stars.forEach(s=>{
+    ctx.beginPath();
+    ctx.arc(s.x,s.y,s.r,0,Math.PI*2);
+    ctx.fill();
+
+    s.y+=s.speed;
+    if(s.y>canvas.height) s.y=0;
+  });
+
+  requestAnimationFrame(draw);
+}
+draw();
